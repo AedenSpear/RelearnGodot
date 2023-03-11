bd77abfdf79b7d9124c78e4120e30b4020f64e26
Brendan <brockbrendan0@gmail.com>
2023-03-11 16:07:24 -0500

    Push all new assets and all new projects.

--- Projects/SimpleRPG/Entites/Player/Player.gd
+++ Projects/SimpleRPG/Entites/Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 # Player movement speed
 export var speed = 75
+var last_direction = Vector2(0, 1)
 
 func _physics_process(delta):
 	# Get player input
@@ -16,3 +17,36 @@ func _physics_process(delta):
 	# Apply movement
 	var movement = speed * direction * delta
 	move_and_collide(movement)
+	
+	# Animate player based on direction
+	animates_player(direction)
+	
+func animates_player(direction: Vector2):
+	if direction != Vector2.ZERO:
+		# gradually update last_direction to counteract the bounce of the analog stick
+		last_direction = 0.5 * last_direction + 0.5 * direction
+		
+		# Choose walk animation based on movement direction
+		var animation = get_animation_direction(last_direction) + "_walk"
+		
+		#Assist for players with analog sticks
+		$Sprite.frames.set_animation_speed(animation, 2 + 8 * direction.length())
+		
+		# Play the walk animation
+		$Sprite.play(animation)
+	else:
+		# Choose idle animation based on last movement direction and play it
+		var animation = get_animation_direction(last_direction) + "_idle"
+		$Sprite.play(animation)
+
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
